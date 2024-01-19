a0612b85121103f2f9c9d3abf2681802e31148a7
Lorisj73 <lorisj73@gmail.com>
2024-01-19 10:31:11 +0100

    Initial Commit

---
+++ Looter/LooterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LooterApp.swift
+//  Looter
+//
+//  Created by Loris JACOB on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct LooterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
