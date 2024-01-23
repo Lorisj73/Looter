1de393d7e357a58fd81ffa44b077e487079f0895
Lorisj73 <lorisj73@gmail.com>
2024-01-23 11:57:09 +0100

    Le filtre marche

--- Looter/AddItemView.swift
+++ Looter/AddItemView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 
 struct AddItemView: View {
     @Environment(\.dismiss) private var dismiss
+    @State var error: Bool = false
     @State var name: String = ""
     @State var rarity: Rarity = Rarity.common
     @State var game: Game = Game.emptyGame
@@ -35,14 +36,23 @@ struct AddItemView: View {
                             Text(String(describing: rarity).capitalized)
                         }
                     }
+                    .alert(isPresented: $error, content: {
+                        Alert(
+                            title: Text("Erreur de format"),
+                            message: Text("Attention: Le nom de l'item dois avoir au moins 3 caractères, le jeu ne peut pas être emptyGame et le type ne peut pas être unknown ")
+                        )
+                    }
+                    )
                 }
                 
                 Section {
                     Picker("Jeu", selection: $game) {
-                        ForEach(0..<availableGames.count, id: \.self) { index in
-                            Text(availableGames[index].name).tag(availableGames[index].id)
-                        }
-                    }
+                                            Text(Game.emptyGame.name).tag(Game.emptyGame)
+                                            ForEach(availableGames) { game in
+                                                Text(game.name)
+                                                    .tag(game)
+                                            }
+                                        }
                     Stepper("Combien: \(quantity)", value: $quantity)
                 }
                 
@@ -75,10 +85,28 @@ struct AddItemView: View {
                     }
                 }
                 Button(action: {
-
-                    let newItem = LootItem(quantity: quantity, name: name, type: type, rarity: rarity, attaStrength: hasAttackStrength ? attackStrength : nil, game: game)
-                    inventory.addItem(item: newItem)
-                    dismiss()
+                    if name.isEmpty {
+                        error = true
+                        return
+                    }
+                    else if name.count < 3 {
+                        error = true
+                        return
+                    }
+                    else if game == Game.emptyGame {
+                        error = true
+                        return
+                    }
+                    else if type == ItemType.unknowed {
+                        error = true
+                        return
+                    }
+                    else {
+                        let newItem = LootItem(quantity: quantity, name: name, type: type, rarity: rarity, attaStrength: hasAttackStrength ? attackStrength : nil, game: game)
+                        inventory.addItem(item: newItem)
+                        dismiss()
+                    }
+                    
                 }, label: {
                     Text("Ajouter")
                 })
